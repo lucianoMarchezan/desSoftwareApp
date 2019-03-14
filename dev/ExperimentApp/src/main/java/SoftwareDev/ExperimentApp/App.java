b4f907351b6aae67b0d95bf2828825ce3ec08ab3
lucianoMarchezan <augustus.marchezan@gmail.com>
2019-03-14 02:52:10 -0300

    creating maven project

---
+++ dev/ExperimentApp/src/main/java/SoftwareDev/ExperimentApp/App.java
@@ -0,0 +1,13 @@
+package SoftwareDev.ExperimentApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
